8aa326f75e49771438962b4fab362ea5106bd739
Roberto Gómez <a25959@svalero.com>
2025-02-13 18:11:32 +0100

    Nombre con logo hecho

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,45 @@
 <script setup lang="ts">
 import { usegatosStore } from '@/stores/gatos';
 
-
-const store = usegatosStore()
-store.findAll()
-
-
+const store = usegatosStore();
+store.findAll();
 </script>
 
 <template>
-    <h1>Listado de gatos</h1>
-    <div v-for="gato in store.gatos">
-        {{ gato.Id_Gato }} {{ gato.Id_Protectora }} {{ gato.Nombre_Gato }} {{ gato.Raza }} {{ gato.Edad }} {{ gato.Esterilizado }} {{ gato.Sexo }} {{ gato.Descripcion_Gato }} {{ gato.Imagen_Gato }}
-    </div>
-
+  <div class="header">
+    <h1 class="logo-container">
+      AdoptArag
+      <svg class="logo-svg" width="40" height="40" viewBox="0 0 160 160" xmlns="http://www.w3.org/2000/svg" fill="none">
+        <circle cx="100" cy="120" r="40" fill="#FF5500" stroke="#3B2F2F" stroke-width="5"/>
+        <circle cx="60" cy="70" r="20" fill="#FF5500" stroke="#3B2F2F" stroke-width="4"/>
+        <circle cx="100" cy="60" r="22" fill="#FF5500" stroke="#3B2F2F" stroke-width="4"/>
+        <circle cx="140" cy="70" r="20" fill="#FF5500" stroke="#3B2F2F" stroke-width="4"/>
+        <circle cx="100" cy="132" r="27" fill="whitesmoke"/>
+      </svg>
+      n
+    </h1>
+  </div>
 </template>
+
+<style scoped>
+.header {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.logo-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #ddd;
+  gap: 5px;
+}
+
+.logo-svg {
+  margin-left: -10px;
+  width: 1em;
+  height: 1em;
+}
+</style>
