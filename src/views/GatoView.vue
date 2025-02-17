7d17053ebc466ddb92317fffb0b1aac4530397a5
Librito21 <a25586@svalero.com>
2025-02-17 10:17:42 +0100

    Imagenes Gatos Card
    Añadidas las imagenes a las cards y modificados los estilos para que se vea mejor

--- src/views/GatoView.vue
+++ src/views/GatoView.vue
@@ -7,28 +7,28 @@ const store = usegatosStore()
 store.fetchGato()
 
 function insertGato() {
-    store.createGato({
-      id_Gato: 1,
-      nombre_Gato: 'JUANJO',
-      raza: 'NEGRO',
-      edad: 3,
-      sexo: 'Macho',
-      id_Protectora: 0,
-      esterilizado: '',
-      descripcion_Gato: '',
-      imagen_Gato: ''
-    })
+  store.createGato({
+    id_Gato: 1,
+    nombre_Gato: 'JUANJO',
+    raza: 'NEGRO',
+    edad: 3,
+    sexo: 'Macho',
+    id_Protectora: 0,
+    esterilizado: '',
+    descripcion_Gato: '',
+    imagen_Gato: ''
+  })
 }
 </script>
 
 <template>
   <v-container>
     <v-row>
-      <v-col v-for="gato in store.gatos" :key="gato.id_Gato">
+      <v-col v-for="gato in store.gatos" :key="gato.id_Gato" cols="1" sm="" md="4" lg="2">
         <GatoCard :gato="gato" />
       </v-col>
     </v-row>
-    
+
     <v-btn color="primary" @click="insertGato">
       Insertar Gato
     </v-btn>
