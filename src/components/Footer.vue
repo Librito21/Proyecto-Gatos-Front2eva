a80b13f742cb684de56497118d5b2dda6cbe836f
Roberto Gómez <a25959@svalero.com>
2025-02-17 16:24:35 +0100

    Hecha estructura de carpetas con las imagenes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,19 +6,19 @@
     <footer class="footer">
     <div class="footer__rrss">
       <a href="insta" class="footer__link">
-        <img src="../../Images/instagram.png" alt="instagram-logo" class="footer__logo">
+        <img src="../../Images/logos/instagram.png" alt="instagram-logo" class="footer__logo">
       </a>
       <a href="linkedin" class="footer__link">
-        <img src="../../Images/linkedin.png" alt="linkedin-logo" class="footer__logo">
+        <img src="../../Images/logos/linkedin.png" alt="linkedin-logo" class="footer__logo">
       </a>
       <a href="X" class="footer__link">
-        <img src="../../Images/twitter.png" alt="twitter-logo" class="footer__logo">
+        <img src="../../Images/logos/twitter.png" alt="twitter-logo" class="footer__logo">
       </a>
       <a href="facebook" class="footer__link">
-        <img src="../../Images/facebook.png" alt="facebook-logo" class="footer__logo">
+        <img src="../../Images/logos/facebook.png" alt="facebook-logo" class="footer__logo">
       </a>
       <a href="youtube" class="footer__link">
-        <img src="../../Images/youtube.png" alt="youtube-logo" class="footer__logo">
+        <img src="../../Images/logos/youtube.png" alt="youtube-logo" class="footer__logo">
       </a>
     </div>
   </footer>
