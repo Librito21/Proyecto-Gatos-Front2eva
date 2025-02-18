9ce7d07bfe2befccfe4fb220f7654f4b7cd1831d
Librito21 <a25586@svalero.com>
2025-02-18 16:19:34 +0100

    Protectoras componente
    Protectoras echa la card como un componente

---
+++ src/components/ProtectorasCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  protectora: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+<v-card class="protectora-card" max-width="500">
+      <v-img class="protectora-card__image" :src="protectora.imagen_Protectora" height="200px" cover></v-img>
+      <v-card-title class="protectora-card__title">{{ protectora.nombre_Protectora }}</v-card-title>
+      <v-card-subtitle class="protectora-card__subtitle"><strong>Dirección:</strong> {{ protectora.direccion}}</v-card-subtitle>
+        <v-card-text class="protectora-card__content">
+        <div class="protectora-card__email"><strong>Correo Electrónico:</strong> {{ protectora.email }}</div>
+        <div class="protectora-card__schedule"><strong>Horario:</strong> {{ protectora.horario_Atención }}</div>
+        <div class="protectora-card__phone"><strong>Teléfono:</strong> {{ protectora.telefono }}</div>
+      </v-card-text>
+    </v-card>
+  </template>
+  
--- src/views/ProtectorasView.vue
+++ src/views/ProtectorasView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ProtectorasCard from '@/components/ProtectorasCard.vue';
 import { useprotectorasStore } from '@/stores/protectoras';
 
 
@@ -14,27 +15,11 @@ store.fetchProtectora()
     <h1>Protectoras</h1>
   </div>
   <v-col v-for="protectora in store.protectoras" :key="protectora.id_Protectora" >
-    <v-card class="protectora-card" max-width="500">
-      <v-img class="protectora-card__image" :src="protectora.imagen_Protectora" height="200px" cover></v-img>
-      <v-card-title class="protectora-card__title">{{ protectora.nombre_Protectora }}</v-card-title>
-      <v-card-subtitle class="protectora-card__subtitle"><strong>Dirección:</strong> {{ protectora.direccion}}</v-card-subtitle>
-        <v-card-text class="protectora-card__content">
-        <div class="protectora-card__email"><strong>Correo Electrónico:</strong> {{ protectora.email }}</div>
-        <div class="protectora-card__schedule"><strong>Horario:</strong> {{ protectora.horario_Atención }}</div>
-        <div class="protectora-card__phone"><strong>Teléfono:</strong> {{ protectora.telefono }}</div>
-      </v-card-text>
-    </v-card>
+    <ProtectorasCard :protectora="protectora"/>
   </v-col>
 </template>
 
 <style scoped lang="scss">
-.v-card-text {
-  padding-left: 14px;
-
-  & .protectora-card__email {
-    
-  }
-}
 
 @media (min-width: 1024px) {
   .about {
