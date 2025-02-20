8d70213a1857bbe83614b090958e2d00a5a31498
Daniel <a25586@svalero.com>
2025-02-20 19:48:29 +0100

    Margin gatos
    Puesto margin para que se vean bien los gatos

--- src/views/GatoView.vue
+++ src/views/GatoView.vue
@@ -9,7 +9,7 @@ store.fetchGato()
 </script>
 
 <template>
-  <v-container fluid class="d-flex" style="align-items: center; justify-content: center;">
+  <v-container fluid class="d-flex" style="margin-top: 110px;">
     <v-row>
       <v-col v-for="gato in store.gatos" :key="gato.id_Gato" cols="1" sm="5" md="4" lg="4">
         <GatoCard :gato="gato" />
